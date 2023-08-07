19c257f72ebf2316ce9dd3759407b4a9beca7fda
Abdoulaye-sys <81588052+Abdoulaye-sys@users.noreply.github.com>
2023-08-07 11:13:14 +0200

    front-done

---
+++ contact.php
@@ -0,0 +1,207 @@
+<?php 
+include("config.php");
+$error="";
+$msg="";
+if(isset($_POST['send']))
+{
+	$name=$_POST['name'];
+	$email=$_POST['email'];
+	$phone=$_POST['phone'];
+	$subject=$_POST['subject'];
+	$message=$_POST['message'];
+	
+	if(!empty($name) && !empty($email) && !empty($phone) && !empty($subject) && !empty($message))
+	{
+		
+		$sql="INSERT INTO contact (name,email,phone,subject,message) VALUES ('$name','$email','$phone','$subject','$message')";
+		   $result=mysqli_query($con, $sql);
+		   if($result){
+			   $msg = "<p class='alert alert-success'>Message envoyé avec succès</p> ";
+		   }
+		   else{
+			   $error = "<p class='alert alert-warning'>Le message n'a pas été envoyé avec succès</p> ";
+		   }
+	}else{
+		$error = "<p class='alert alert-warning'>Veuillez remplir tous les champs</p>";
+	}
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Meta Tags -->
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<link rel="shortcut icon" href="images/favicon.ico">
+
+<!--	Fonts
+	========================================================-->
+<link href="https://fonts.googleapis.com/css?family=Muli:400,400i,500,600,700&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Comfortaa:400,700" rel="stylesheet">
+<!--	Css Link
+	========================================================-->
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap-slider.css">
+<link rel="stylesheet" type="text/css" href="css/jquery-ui.css">
+<link rel="stylesheet" type="text/css" href="css/layerslider.css">
+<link rel="stylesheet" type="text/css" href="css/color.css" id="color-change">
+<link rel="stylesheet" type="text/css" href="css/owl.carousel.min.css">
+<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="fonts/flaticon/flaticon.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+
+<!--	Title
+	=========================================================-->
+<title>Atypik House</title>
+</head>
+<body>
+
+<!--	Page Loader
+=============================================================
+<div class="page-loader position-fixed z-index-9999 w-100 bg-white vh-100">
+	<div class="d-flex justify-content-center y-middle position-relative">
+	  <div class="spinner-border" role="status">
+		<span class="sr-only">Loading...</span>
+	  </div>
+	</div>
+</div>
+--> 
+<div id="page-wrapper">
+    <div class="row"> 
+        <!--	Header start  -->
+		<?php include("include/header.php");?>
+        <!--	Header end  -->
+        
+        <!--	Banner -->
+        <!-- <div class="banner-full-row page-banner" style="background-image:url('images/breadcromb.jpg');">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h2 class="page-name float-left text-white text-uppercase mt-1 mb-0"><b>Contact</b></h2>
+                    </div>
+                    <div class="col-md-6">
+                        <nav aria-label="breadcrumb" class="float-left float-md-right">
+                            <ol class="breadcrumb bg-transparent m-0 p-0">
+                                <li class="breadcrumb-item text-white"><a href="#">Home</a></li>
+                                <li class="breadcrumb-item active">Contact</li>
+                            </ol>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div> -->
+        <!--	Banner -->
+        <!--	Contact Information -->
+        <div class="full-row">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4 mb-5 bg-secondary">
+                        <div class="contact-info">
+                            <h3 class="mb-4 mt-4 text-white">Contacts</h3>
+							
+                            <ul>
+                                <li class="d-flex mb-4"> <i class="fas fa-map-marker-alt text-white mr-2 font-13 mt-1"></i>
+                                    <div class="contact-address">
+                                        <h5 class="text-white">Adresse</h5>
+                                        <span class="text-white">11 allée des alpes, Vincennes</span> <br>
+										<span class="text-white">11 allée des alpes, Vincennes</span</span>
+										</div>
+                                </li>
+                                <li class="d-flex mb-4"> <i class="fas fa-phone-alt text-white mr-2 font-13 mt-1"></i>
+                                    <div class="contact-address">
+                                        <h5 class="text-white">Appelez Nous</h5>
+                                        <span class="d-table text-white">+1 234-567-8910</span>
+										<span class="text-white">+1 243-765-4321 </span>
+									</div>
+                                </li>
+                                <li class="d-flex mb-4"> <i class="fas fa-envelope text-white mr-2 font-13 mt-1"></i>
+                                    <div class="contact-address">
+                                        <h5 class="text-white">Adresse Email</h5>
+										<span class="d-table text-white">helpline@realestatest.com</span>
+										<span class="text-white">helpline@atypikhouse.com</span>
+										</div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+					<div class="col-lg-1"></div>
+                    <div class="col-md-12 col-lg-7">
+						<div class="container">
+                        <div class="row">
+							<div class="col-lg-12">
+								<h2 class="text-secondary double-down-line text-center mb-5">Entrer en contact</h2>
+								<?php echo $msg; ?><?php echo $error; ?>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-12">
+								<form class="w-100" action="#" method="post">
+									<div class="row">
+										<div class="row mb-4">
+											<div class="form-group col-lg-6">
+												<input type="text"  name="name" class="form-control" placeholder="Votre Nom*">
+											</div>
+											<div class="form-group col-lg-6">
+												<input type="text"  name="email" class="form-control" placeholder="Adresse Email*">
+											</div>
+											<div class="form-group col-lg-6">
+												<input type="text"  name="phone" class="form-control" placeholder="Telephone" maxlength="10">
+											</div>
+											<div class="form-group col-lg-6">
+												<input type="text" name="subject"  class="form-control" placeholder="Sujet">
+											</div>
+											<div class="col-lg-12">
+												<div class="form-group">
+													<textarea name="message" class="form-control" rows="5" placeholder="Tapez votre message..."></textarea>
+												</div>
+											</div>
+										</div>
+										<button type="submit" value="send message" name="send" class="btn btn-success">Envoyez le message</button>
+									</div>
+								</form>
+							</div>
+						</div>
+						</div>
+					</div>
+                </div>
+            </div>
+        </div>
+        <!--	Contact Inforamtion -->
+        
+        
+        <!--	Footer   start-->
+		<?php include("include/footer.php");?>
+		<!--	Footer   start-->
+        
+        <!-- Scroll to top --> 
+        <a href="#" class="bg-secondary text-white hover-text-secondary" id="scroll"><i class="fas fa-angle-up"></i></a> 
+        <!-- End Scroll To top --> 
+    </div>
+</div>
+<!-- Wrapper End --> 
+
+<!--	Js Link
+============================================================--> 
+<script src="js/jquery.min.js"></script> 
+<!--jQuery Layer Slider --> 
+<script src="js/greensock.js"></script> 
+<script src="js/layerslider.transitions.js"></script> 
+<script src="js/layerslider.kreaturamedia.jquery.js"></script> 
+<!--jQuery Layer Slider --> 
+<script src="js/popper.min.js"></script> 
+<script src="js/bootstrap.min.js"></script> 
+<script src="js/owl.carousel.min.js"></script> 
+<script src="js/tmpl.js"></script> 
+<script src="js/jquery.dependClass-0.1.js"></script> 
+<script src="js/draggable-0.1.js"></script> 
+<script src="js/jquery.slider.js"></script> 
+<script src="js/wow.js"></script> 
+<script src="js/jquery.cookie.js"></script> 
+<script src="js/custom.js"></script>  
+
+</body>
+</html>
---
+++ include/footer.php
@@ -0,0 +1,67 @@
+<footer class="full-row bg-secondary p-0">
+            <div class="container">
+                <div  class="row">
+                    <div class="col-lg-12">
+                        <div class="divider py-40">
+                            <div class="row">
+                                <div class="col-md-12 col-lg-4">
+                                    <div class="footer-widget mb-4">
+                                        <div class="footer-logo mb-4"> <a href="#"><img class="logo-bottom" src="images/logo/atypikhouse-logos_black .png" alt="image"></a> </div>
+                                        <p class="pb-20 text-white">Chez Atypik House, nous mettons un point d'honneur à rendre l'expérience de location agréable et sans stress pour nos clients. Notre approche personnalisée, notre analyse minutieuse des propriétés et notre soutien tout au long du processus garantissent que vous trouverez le logement idéal qui répond à vos besoins et dépasse vos attentes. Laissez-nous vous accompagner dans votre recherche du logement atypique de vos rêves !</p>
+										
+                                        </div>
+                                </div>
+                                <div class="col-md-12 col-lg-8">
+                                    <div class="row">
+                                        <div class="col-md-4 col-lg-4">
+                                            <div class="footer-widget footer-nav mb-4">
+                                                <h4 class="widget-title text-white double-down-line-left position-relative">Support</h4>
+                                                <ul class="hover-text-primary">
+                                                    <li><a href="#" class="text-white">Forum</a></li>
+                                                    <li><a href="#" class="text-white">Termes et conditions</a></li>
+                                                    <li><a href="#" class="text-white">Foire aux questions</a></li>
+                                                    <li><a href="contact.php" class="text-white">Contact</a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4 col-lg-4">
+                                            <div class="footer-widget footer-nav mb-4">
+                                                <h4 class="widget-title text-white double-down-line-left position-relative">Liens rapides</h4>
+                                                <ul class="hover-text-primary">
+                                                    <li><a href="about.php" class="text-white">À propos de nous</a></li>
+                                                    <li><a href="#" class="text-white">Propriété en vedette</a></li>
+                                                    <li><a href="#" class="text-white">Soumettre la propriété</a></li>
+                                                    <li><a href="agent.php" class="text-white">Nos Agents</a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4 col-lg-4">
+                                            <div class="footer-widget">
+                                                <h4 class="widget-title text-white double-down-line-left position-relative">Contactez-nous</h4>
+                                                <ul class="text-white">
+                                                    <li class="hover-text-primary"><i class="fas fa-map-marker-alt text-white mr-2 font-13 mt-1"></i>11 allée des alpes , Villiers</li>
+                                                    <li class="hover-text-primary"> <i class="fas fa-phone-alt text-white mr-2 font-13 mt-1"></i>+1 234-567-8910</li>
+													<li class="hover-text-primary"> <i class="fas fa-phone-alt text-white mr-2 font-13 mt-1"></i>+1 243-765-4321</li>
+                                                    <li class="hover-text-primary"><i class="fas fa-envelope text-white mr-2 font-13 mt-1"></i>support@atypikhouse.com</li>
+                                                </ul>
+                                            </div>
+                                            <div class="footer-widget media-widget mt-4 text-white hover-text-primary"> <a href="#"><i class="fab fa-facebook-f"></i></a> <a href="#"><i class="fab fa-twitter"></i></a> <a href="#"><i class="fab fa-google-plus-g"></i></a> <a href="#"><i class="fab fa-linkedin-in"></i></a> <a href="#"><i class="fas fa-rss"></i></a> </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row copyright">
+                    <div class="col-sm-6"> <span class="text-white">© <?php echo date('Y');?> Atypik House Website - Developer Par Abdoulaye Ba</span> </div>
+                    <div class="col-sm-6">
+                        <ul class="line-menu text-white hover-text-primary float-right">
+                            <li><a href="#">Politique de confidentialité</a></li>
+                            <li>|</li>
+                            <li><a href="#"> Site Map</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
